ffc30b95d93a9c701024fb4ef7f928902f58280e
JoelVCrasta <loejstarc@gmail.com>
2026-02-10 11:51:42 +0530

    added classname prop

--- components/IconButton.tsx
+++ components/IconButton.tsx
@@ -1,14 +1,15 @@
 interface IconButtonProps {
   icon: React.ReactNode;
+  className?: string;
 
   onClick: () => void;
 }
 
-const IconButton = ({ icon, onClick }: IconButtonProps) => {
+const IconButton = ({ icon, onClick, className }: IconButtonProps) => {
   return (
     <button
       onClick={onClick}
-      className={`p-1 rounded-md transition-colors duration-200 hover:bg-accent-secondary`}
+      className={`p-1 rounded-md transition-colors duration-200 hover:bg-accent-secondary ${className ? className : ""}`}
     >
       {icon}
     </button>
