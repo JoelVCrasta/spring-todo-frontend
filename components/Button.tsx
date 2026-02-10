28ed36132096a27867b7cef9b7fcd2f44ad70262
JoelVCrasta <loejstarc@gmail.com>
2026-02-10 11:54:46 +0530

    updated to make it usable without title and ui changes

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,5 +1,5 @@
 interface ButtonProps {
-  title: string;
+  title?: string;
   icon: React.ReactNode;
   type?: "outline" | "solid";
   onClick: () => void;
@@ -9,12 +9,12 @@ const Button = ({ title, icon, onClick, type = "solid" }: ButtonProps) => {
   return (
     <button
       onClick={onClick}
-      className={`flex items-center gap-2 px-3 rounded-md transition-colors duration-200 hover:bg-accent-secondary ${
+      className={`flex items-center gap-2 px-3 rounded-md transition-colors duration-200 hover:bg-accent-secondary h-full ${
         type === "outline" ? "border border-highlight" : "bg-accent-primary"
       }`}
     >
-      <div className="text-gray-400">{icon}</div>
-      <span className="text-sm font-medium">{title}</span>
+      <div className="text-foreground">{icon}</div>
+      {title && <span className="text-sm font-medium">{title}</span>}
     </button>
   );
 };
