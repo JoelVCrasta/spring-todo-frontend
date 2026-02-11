f70cfce1a2a7bd9459d2e3707227b3a92c2940fe
JoelVCrasta <loejstarc@gmail.com>
2026-02-11 15:31:18 +0530

    added menu header orientation

--- components/MenuHeader.tsx
+++ components/MenuHeader.tsx
@@ -3,15 +3,29 @@ import IconButton from "./IconButton";
 interface MenuHeaderProps {
   title?: string;
   icon?: React.ReactNode;
+  right?: boolean;
   onToggle: () => void;
 }
 
-const MenuHeader = ({ title, icon, onToggle }: MenuHeaderProps) => {
+const MenuHeader = ({
+  title,
+  icon,
+  right = false,
+  onToggle,
+}: MenuHeaderProps) => {
   return (
     <div className="flex items-center justify-between">
-      <h2 className="font-bold text-2xl text-foreground">{title}</h2>
-
-      <IconButton icon={icon} onClick={onToggle} />
+      {right ? (
+        <>
+          <IconButton icon={icon} onClick={onToggle} />
+          <h2 className="font-bold text-2xl text-foreground">{title}</h2>
+        </>
+      ) : (
+        <>
+          <h2 className="font-bold text-2xl text-foreground">{title}</h2>
+          <IconButton icon={icon} onClick={onToggle} />
+        </>
+      )}
     </div>
   );
 };
