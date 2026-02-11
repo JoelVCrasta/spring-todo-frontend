df9214a16077f207d7c7ec8c131a70eb561b541d
JoelVCrasta <loejstarc@gmail.com>
2026-02-11 15:30:12 +0530

    updated button to have better types

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,19 +1,37 @@
 interface ButtonProps {
   title?: string;
-  icon: React.ReactNode;
-  type?: "outline" | "solid";
+  icon?: React.ReactNode;
+  type?: "button" | "submit" | "reset";
+  variant?: "outline" | "solid-1" | "solid-2";
+  wide?: boolean;
   onClick: () => void;
 }
 
-const Button = ({ title, icon, onClick, type = "solid" }: ButtonProps) => {
+const Button = ({
+  title,
+  icon,
+  onClick,
+  type = "button",
+  variant = "solid-1",
+  wide = false,
+}: ButtonProps) => {
+  const variantClasses = {
+    outline:
+      "border border-highlight text-foreground hover:bg-accent-secondary",
+    "solid-1": "bg-accent-primary text-foreground hover:bg-accent-primary/80",
+    "solid-2":
+      "bg-accent-secondary text-foreground hover:bg-accent-secondary/80",
+  };
+
+  const buttonClass = variantClasses[variant];
+
   return (
     <button
       onClick={onClick}
-      className={`flex items-center gap-2 px-3 rounded-md transition-colors duration-200 hover:bg-accent-secondary h-full ${
-        type === "outline" ? "border border-highlight" : "bg-accent-primary"
-      }`}
+      type={type}
+      className={`flex items-center justify-center gap-2 px-3 rounded-md transition-colors duration-200 hover:bg-accent-secondary h-11.5 ${buttonClass} ${wide ? "w-full" : ""}`}
     >
-      <div className="text-foreground">{icon}</div>
+      {icon && <div className="text-foreground">{icon}</div>}
       {title && <span className="text-sm font-medium">{title}</span>}
     </button>
   );
